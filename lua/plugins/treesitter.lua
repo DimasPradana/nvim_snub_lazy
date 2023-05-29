6250fd4c401486a97bd9f0b173b282a03bae5cd2
DimasPradana <deemaazbaek@gmail.com>
2023-05-29 18:51:49 +0700

    add ctrl-space,af,as,etc to select on view mode

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -35,6 +35,13 @@ return {
     -- autotag
     {
       "windwp/nvim-ts-autotag",
+      config = function()
+        --[[ require("nvim-ts-autotag").setup({
+          enable = true,
+          filetypes = { "html", "xml", "javascriptreact", "typescriptreact", "svelte", "javascript", "typescript" },
+        }) ]]
+        require("nvim-ts-autotag").setup()
+      end,
     },
 
     -- docs
@@ -61,7 +68,10 @@ return {
   opts = {
     highlight = { enable = true },
     indent = { enable = true },
-    -- autotag = { enable = true },
+    --[[ autotag = {
+      enable = true,
+      filetypes = { "html", "xml", "javascriptreact", "typescriptreact", "svelte", "javascript", "typescript" },
+    }, ]]
     context_commentstring = { enable = true, enable_autocmd = false },
     refactor = {
       highlight_definitions = { enable = true },
@@ -144,7 +154,16 @@ return {
         set_jumps = true, -- whether to set jumps in the jumplist
         goto_next_start = {
           ["]m"] = "@function.outer",
-          ["]]"] = "@class.outer",
+          ["]]"] = { query = "@class.outer", desc = "Next class start" },
+          --
+          -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queires.
+          ["]o"] = "@loop.*",
+          -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
+          --
+          -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
+          -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
+          ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
+          ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
         },
         goto_next_end = {
           ["]M"] = "@function.outer",
@@ -158,9 +177,34 @@ return {
           ["[M"] = "@function.outer",
           ["[]"] = "@class.outer",
         },
+        -- Below will go to either the start or the end, whichever is closer.
+        -- Use if you want more granular movements
+        -- Make it even more gradual by adding multiple queries and regex.
+        goto_next = {
+          ["]d"] = "@conditional.outer",
+        },
+        goto_previous = {
+          ["[d"] = "@conditional.outer",
+        },
       },
-      swap = {},
-      lsp_interop = {},
+      swap = {
+        enable = true,
+        swap_next = {
+          ["<leader>a"] = "@parameter.inner",
+        },
+        swap_previous = {
+          ["<leader>A"] = "@parameter.inner",
+        },
+      },
+      lsp_interop = {
+        enable = true,
+        border = "none",
+        floating_preview_opts = {},
+        peek_definition_code = {
+          ["<leader>df"] = "@function.outer",
+          ["<leader>dF"] = "@class.outer",
+        },
+      },
       --[[ builtn-in :
         @block.inner
         @block.outer
@@ -257,7 +301,7 @@ return {
   ---@param opts TSConfig
   config = function(_, opts)
     require("nvim-treesitter.configs").setup(opts)
-    require("nvim-ts-autotag").setup({})
+    -- require("nvim-ts-autotag").setup({})
     require("treesitter-context").setup({
       enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
       max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
