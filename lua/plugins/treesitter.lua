b4fce3931d11504e8a4eac4427ccc3f4173ee669
DimasPradana <deemaazbaek@gmail.com>
2023-05-29 13:02:49 +0700

    add textobject keymaps, and ctrl-space on selection on treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -129,8 +129,15 @@ return {
           ["af"] = "@function.outer",
           ["if"] = "@function.inner",
           ["ac"] = "@class.outer",
-          ["ic"] = "@class.inner",
+          ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+          ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
         },
+        selection_modes = {
+          ["@parameter.outer"] = "v", -- charwise
+          ["@function.outer"] = "V", -- linewise
+          ["@class.outer"] = "<c-v>", -- blockwise
+        },
+        include_surrounding_whitespace = false,
       },
       move = {
         enable = true,
