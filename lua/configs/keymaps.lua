6e51b571c24af32e10acf48881f4bddb9507aba7
DimasPradana <deemaazbaek@gmail.com>
2023-07-06 19:55:52 +0700

    add custom key and util

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -2,6 +2,49 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local Utils = require("configs.util")
+local M = {}
+
+local exprnnoremap = Utils.exprnnoremap
+local nnoremap = Utils.nnoremap
+-- local vnoremap = Utils.vnoremap
+-- local xnoremap = Utils.xnoremap
+local inoremap = Utils.inoremap
+local tnoremap = Utils.tnoremap
+local cnoremap = Utils.cnoremap
+-- local nmap = Utils.tnoremap
+
+-- {{{ custom key
+function M.snubCustomKey(key, mode)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
+-- center next, prev and line concatenate
+nnoremap("n", "nzzzv")
+nnoremap("N", "Nzzzv")
+
+-- word wraps
+exprnnoremap("k", "v:count == 0 ? 'gk':'k'")
+exprnnoremap("j", "v:count == 0 ? 'gj':'j'")
+
+-- tambah baris kosong diatas
+vim.keymap.set("n", "<A-O>", function()
+  M.snubCustomKey("O", "n")
+  M.snubCustomKey("<esc>", "n")
+  M.snubCustomKey("gj", "n")
+  -- snubCustomKey("zz", "n")
+end)
+
+-- tambah baris kosong dibawah
+vim.keymap.set("n", "<A-o>", function()
+  M.snubCustomKey("o", "n")
+  M.snubCustomKey("<esc>", "n")
+  M.snubCustomKey("gk", "n")
+  -- snubCustomKey("zz", "n")
+end)
+
+-- }}}
+
 -- escape keymap
 vim.keymap.set("i", "jk", "<esc>")
 vim.keymap.set("t", "jk", "<C-\\><C-n>")
@@ -23,3 +66,5 @@ vim.keymap.set("n", "<A-j>", "<CMD>bd!<CR>")
 vim.keymap.set("n", "<F12>", "<CMD>vsplit | terminal<CR>")
 vim.keymap.set("x", "<F12>", "<CMD>vsplit | terminal<CR>")
 vim.keymap.set("i", "<F12>", "<CMD>vsplit | terminal<CR>")
+
+-- vim:fileencoding=utf-8:ft=lua:foldmethod=marker
--- lua/configs/util.lua
+++ lua/configs/util.lua
@@ -1,4 +1,4 @@
-local Util = require("lazy.core.util")
+-- local Utils = require("lazy.core.util")
 
 local M = {}
 
@@ -64,5 +64,54 @@ function M.get_root()
   ---@cast root string
   return root
 end
+
+-- :help map-modes
+function M.map(mode, lhs, rhs)
+  vim.api.nvim_set_keymap(mode, lhs, rhs, { silent = true })
+end
+
+function M.noremap(mode, lhs, rhs)
+  vim.api.nvim_set_keymap(mode, lhs, rhs, { noremap = true, silent = true })
+end
+
+function M.exprnoremap(mode, lhs, rhs)
+  vim.api.nvim_set_keymap(mode, lhs, rhs, { noremap = true, silent = true, expr = true })
+end
+
+--[[ function M.custom_cwd()
+	if vim.loop.cwd() == vim.loop.os_homedir() then
+		return vim.fn.expand("%:p:h")
+	end
+	return vim.loop.cwd()
+end ]]
+
+--[[ function M.eval()
+	-- cara cek api pakai :lua = nama api, misal vim.api.nvim_eval_statusline("%f", {}).str
+	local file_path = vim.api.nvim_eval_statusline("%f", {}).str
+	local modified = vim.api.nvim_eval_statusline("%M", {}).str == "+" and "⦿" or "" -- jika ada modifikasi munculkan icon "⦿", jika tidak string kosong
+
+	file_path = file_path:gsub("/", " ➤  ")
+
+	return string.format("%s %s", file_path, modified)
+end ]]
+
+-- stylua: ignore start
+function M.nmap(lhs, rhs) M.map('n', lhs, rhs) end
+
+function M.nnoremap(lhs, rhs) M.noremap('n', lhs, rhs) end
+
+function M.vnoremap(lhs, rhs) M.noremap('v', lhs, rhs) end
+
+function M.xnoremap(lhs, rhs) M.noremap('x', lhs, rhs) end
+
+function M.inoremap(lhs, rhs) M.noremap('i', lhs, rhs) end
+
+function M.tnoremap(lhs, rhs) M.noremap('t', lhs, rhs) end
+
+function M.cnoremap(lhs, rhs) M.noremap('c', lhs, rhs) end
+
+function M.exprinoremap(lhs, rhs) M.exprnoremap('i', lhs, rhs) end
+
+function M.exprnnoremap(lhs, rhs) M.exprnoremap('n', lhs, rhs) end
 
 return M
